8979d462992b06e624ec2041ec7b96d87175a1bf
luankrzyzaniak <luankrzyzaniak@gmail.com>
2023-11-17 14:21:41 -0300

    added x

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,15 +6,16 @@ const cors = require('cors');
 // db.cursos.push({ ... })
 
 
-// para remover da lista, use splice passando o índice do elemento e a quantidade de
-// elementos a serem removidos. Exemnplo para remover o primeiro elemento:
-// db.cursos.splice(0, 1) remove um elemento a partir do índice 0
-
 const express = require("express");
 const app = express();
 const port = 6969;
 
 app.use(cors());
+
+const pgp = require("pg-promise")({});
+const usuario = "postgres";
+const senha = "postgres";
+const db = pgp(`postgres://${usuario}:${senha}@localhost:5432/uffs_curso`);
 
 app.use(bodyParser.json());
 
@@ -30,9 +31,35 @@ app.post("/follow"), (req,res) =>{
     }
 }
 
+app.post("/sendmajor", async (req, res) => {
+    try {
+        const mname = req.body.mname;
+        const mdesc = req.body.mdesc;
+
+        if (mname === '' || mdesc === '') {
+            console.log(error);
+            res.status(400);
+            return
+        }
+
+        db.none(
+            "INSERT INTO major (mname, mdesc) VALUES ($1, $2);",
+            [mname, mdesc]
+        );
+        res.sendStatus(200);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(400);
+    }
+});
+
 app.get("/schedule", (req,res) =>{ //receber info
     res.json(schedule);
 });
+
+app.get("/getmajors") {
+    
+}
 
 app.listen(port,() => {
     console.log(`Server opened on PORT ${port} :)`);
--- frontend/src/components/Insertmajor.jsx
+++ frontend/src/components/Insertmajor.jsx
@@ -1,8 +1,61 @@
+import { Button } from "@mui/material";
 import Banner from "./Banner";
+import axios from "axios";
+
 function Insertmajor (){
+
+    async function sendMajor (){
+
+        axios.post("http://localhost:6969/sendmajor", {
+            mname: document.forms[0].elements[0].value,
+            mdesc: document.forms[0].elements[1].value
+        })
+        .then(() => {
+           console.log('worked');
+        })
+        .catch((err) => {
+            if(err) console.log(err);
+        });
+    }
+
+    const[majors,setMajors] = useState({});
+
+    useEffect(() =>{
+        axios.get("http://localhost:6969/getmajors")
+        .then((query) => {
+           setMajors(query.data);
+        })
+        .catch((err) => {
+            if(err) throw(err);
+        });
+    }, []);
+
     return (
         <>
             <Banner/>
+            <div className="divheader" style={{paddingTop:'12rem'}}>
+                <header>
+                    <h1>CADASTRAR CURSO</h1>
+                    <p>Preencha o formulário abaixo para inserir o curso.</p>
+                    <form>
+                        <div>
+                            <label for="maname">Nome do curso:</label><br/>
+                            <input maxlenght='32' type="text" id="mname" name="mname"/><br/>
+                        </div>
+                        <div>
+                            <label for="mdesc">Descrição do curso:</label><br/>
+                            <input maxlenght='255' type="text" id="mdesc" name="mdesc"/><br/>
+                        </div>
+                    </form>
+                    <div style={{paddingTop:"1rem"}}>
+                            <a id="futurobotao" onClick={sendMajor}>Cadastrar</a>
+                    </div>
+                    <div>
+                        <h3 style={{paddingTop:"2rem"}}>Cursos da UFFS </h3>
+
+                    </div>
+                </header>
+            </div>
         </>
     );
 }
